d4e0cf321a88343eedd6e02411f2afcd3d386bc3
tj1023 <htjeong1023@naver.com>
2024-12-02 15:21:34 +0900

    59. Pitch in Multiplayer

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -168,6 +168,11 @@ void ABlasterCharacter::AimOffset(float DeltaTime)
 	}
 
 	AO_Pitch = GetBaseAimRotation().Pitch;
+	if (AO_Pitch > 90.0f && !IsLocallyControlled()) {
+		FVector2D InRange(270.f, 360.f);
+		FVector2D OutRange(-90.f, 0.f);
+		AO_Pitch = FMath::GetMappedRangeValueClamped(InRange, OutRange, AO_Pitch);
+	}
 }
 
 void ABlasterCharacter::ServerEquipButtonPressed_Implementation()
